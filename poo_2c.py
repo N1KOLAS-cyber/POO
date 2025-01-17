274f270dd8287841643bfa5adfab77ae279490d2
N1KOLAS-cyber <gamboanicolas58@gmail.com>
2025-01-17 13:31:01 -0600

    matadero

--- poo_2c.py
+++ poo_2c.py
@@ -1,108 +1,118 @@
 class Personaje:
-    #Atributos de la clase
-    # nombre = 'Default'
-    # fuerza = 0
-    # inteligencia = 0
-    # defensa = 0
-    # vida = 0
-    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
-        self.__nombre = nombre
-        self.__fuerza = fuerza
+    # Constructor de la clase
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):  # Corregido a __init__
+        self.nombre = nombre
+        self.fuerza = fuerza
         self.inteligencia = inteligencia
-        self.__defensa = defensa
-        self.__vida = vida
-    #¿Qué es self? Es una referencia al mismo objeto
-    #¿Qué es el método init? Constructor que inicializa
-    #atributos de un objeto
-    #¿Por qué se usa doble guión bajo? Dunder. Porque es
-    #método mágico.
-    #¿Cuándo se ejecuta el método init? Autom. al crear
-    #una nueva instancia
-    
+        self.defensa = defensa
+        self.vida = vida
+
     def imprimir_atributos(self):
-        print(self.__nombre)
-        print("-Fuerza:",self.__fuerza)
-        print("-Inteligencia:",self.inteligencia)
-        print("-Defensa:",self.__defensa)
-        print("-Vida:",self.__vida)
+        print(self.nombre)
+        print("-Fuerza:", self.fuerza)
+        print("-Inteligencia:", self.inteligencia)
+        print("-Defensa:", self.defensa)
+        print("-Vida:", self.vida)
     
-    def subir_nivel(self,fuerza, inteligencia, defensa):
-        self.__fuerza = self.__fuerza + fuerza
-        self.inteligencia = self.inteligencia + inteligencia
-        self.__defensa = self.__defensa + defensa
+    def subir_nivel(self, fuerza, inteligencia, defensa):
+        self.fuerza += fuerza
+        self.inteligencia += inteligencia
+        self.defensa += defensa
     
-    def esta_vivo(self):        
-        return self.__vida > 0
-        
+    def esta_vivo(self):
+        return self.vida > 0
+    
     def morir(self):
-        self.__vida = 0
-        print(self.__nombre,"ha muerto")
+        self.vida = 0
+        print(self.nombre, "Ha muerto")
+        return self.vida <= 0
+    
+    def dmg(self, enemigo):
+        # Si la defensa del enemigo es mayor o igual que la fuerza del atacante, el daño es 0
+        return max(0, self.fuerza - enemigo.defensa)
+    
+    def atacar(self, enemigo):
+        daño = self.dmg(enemigo)
+        # Asegurarse de que la vida no baje de cero
+        enemigo.vida = max(0, enemigo.vida - daño)
+        print(self.nombre, "Ha realizado", daño, "puntos de daño a", enemigo.nombre)
+        print("Vida de", enemigo.nombre, "es", enemigo.vida)
         
+class Guerrero(Personaje):
+    
+    # Sobreescribir el constructor
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, espada):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.espada = espada
+        
+    # Sobreescribir impresión de atributos
+    def imprimir_atributos(self):
+        super().imprimir_atributos()
+        print("-Espada:", self.espada)
+
+    # Sobreescribir el cálculo del daño
     def dañar(self, enemigo):
-        return self.__fuerza - enemigo.__defensa
+        return self.fuerza * self.espada - enemigo.defensa 
     
-    def atacar(self,enemigo):
-        daño = self.dañar(enemigo)
-        if daño < 0 :
-            daño = 0
-        if enemigo.__vida - daño < 0:
-            enemigo.__vida
+    # Escoger navaja
+    def escoger_navaja(self):
+        opcion = int(input("Escoge sabiamente:\n(1) Navaja suiza, daño 10.\n(2) Navaja pioja, daño 4.\n>>>>>> "))
+        if opcion == 1:
+            self.espada = 10  
+        elif opcion == 2:
+            self.espada = 6
         else:
-            enemigo.__vida = enemigo.__vida - daño
-        print(self.__nombre,"ha realizado",daño,"puntos de daño a", enemigo.__nombre)
-        print("Vida de",enemigo.__nombre,"es",enemigo.__vida)
-    
-    class Guerrero (Personaje):
-     pass 
-     #sobreescribir el constructor
-     def __init__(self,nombre,fuerza, inteligencia, defensa, vida, espada):
-         super().__init__(self, nombre, fuerza, inteligencia, defensa, vida)
-         self.espada = espada
-    
-    arturoSuarez = Guerrero("Arturo Suarez", 12, 3000,2,100,.5)
-    arturoSuarez.imprimir_atributos()
-    print("el valor de espada es: ", arturoSuarez.espada)
+            print("Valor inválido, intente nuevamente.")
+            # Vuelve a ejecutar el método escoger navaja
+            self.escoger_navaja()
 
+#################################################################
+class Mago(Personaje):
     
-    def get_vida(self):
-        return self.__vida
-    
-    def set_vida(self, vida):
-        self.__vida = vida  
-        if self.__vida <= 0 :
-            self.morir()
-            
+    # Sobreescribir el constructor
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, libro):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.espada = libro
         
-        
-           
-#Variable del constructor 
-mi_personaje = Personaje("EsteBandido", 100, 50, 45, 100)
-mi_enemigo = Personaje("Ángel",70,100,70,100)
-print(mi_personaje.get_vida())
-mi_personaje.set_vida(-5)
-print(mi_personaje.get_vida())
-mi_personaje._Personaje__vida = -50
-mi_personaje.imprimir_atributos()
+    # Sobreescribir impresión de atributos
+    def imprimir_atributos(self):
+        super().imprimir_atributos()
+        print("-libro:", self.libro)
 
+    # Sobreescribir el cálculo del daño
+    def dañar(self, enemigo):
+        return self.inteligencia * self.libro - enemigo.defensa 
+    
+    # Escoger libro
+    def escoger_libro(self):
+        opcion = int(input("Escoge el libro de la sabiduria:\n(1) El principito, daño 10.\n(2) crepúculo, daño 6.\n>>>>>> "))
+        if opcion == 1:
+            self.libro = 10  
+        elif opcion == 2:
+            self.libro = 6
+        else:
+            print("Valor inválido, intente nuevamente.")
+            # Vuelve a ejecutar el método escoger navaja
+            self.escoger_libro()
 
-
-#mi_personaje.imprimir_atributos()
-#mi_personaje.atacar(mi_enemigo)
-# mi_personaje.morir()
-#print(mi_personaje.esta_vivo())
-# mi_personaje.subir_nivel(15,5,10)
-# print("Valores actualizados")
-# mi_personaje.imprimir_atributos()
-
-#Modificando valores de los atributos
-# mi_personaje.__nombre = "EstebanDido"
-# mi_personaje.__fuerza = 300
-# mi_personaje.inteligencia = -2
-# mi_personaje.__defensa = 30
-# mi_personaje.__vida = 2
-
-# print("El nombre de mi personaje es: ",mi_personaje.__nombre)
-# print("El fuerza de mi personaje es: ",mi_personaje.__fuerza)
-# print("El inteligencia de mi personaje es: ",mi_personaje.inteligencia)
-# print("El defensa de mi personaje es: ",mi_personaje.__defensa)
-# print("El vida de mi personaje es: ",mi_personaje.__vida)
+# Crear objeto Guerrero
+#arturoSuarez = Guerrero("Arturo Suárez", 12, 3000, 2, 100, 0.5)
+#arturoSuarez.escoger_navaja()
+#arturoSuarez.imprimir_atributos(
+# creando todos los objetos 
+persona = Personaje("Angel Suarez", 20, 15, 10, 100)
+arturosuarez = Guerrero("Arturo suarez ", 20, 15, 10,100,5)
+gandalf = Mago ("Gandalf", 20, 15,10,100,5)
+#atributos antes de la tragedia 
+persona.imprimir_atributos()
+arturosuarez.imprimir_atributos()
+gandalf.imprimir_atributos()
+#Ataques sin piedad 
+persona.atacar(arturosuarez)
+arturosuarez.atacar(gandalf)
+gandalf.atacar(gandalf)
+gandalf.atacar(persona)
+#atributos despues de la tragedia
+persona.imprimir_atributo()
+arturosuarez.imprimir_atributos()
+gandalf.imprimir_atributos()
